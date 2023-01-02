d156b2135ab851eff1b0adf3dfbb6e8c279fa6b6
Adrian Bece <adrianbece@live.com>
2023-01-03 00:05:52 +0100

    Revert

--- src/modules/list/components/Card.svelte
+++ src/modules/list/components/Card.svelte
@@ -75,8 +75,7 @@
     href={isLoading ? '#' : `/album/${id}`}
     class:card--clicked={isClickedOn}
     class="card__link"
-    data-sveltekit-preload-code
-    data-sveltekit-preload-data
+    sveltekit:prefetch
   >
     <figure class="card__figure">
       <i class="fa-solid fa-compact-disc card__spinner" />
